cc85c49ed15189f689c4acb1f9f189f3c850430d
ayunawani <ayu.nawani@gmail.com>
2017-05-04 20:53:53 +0530

    basic login ui added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import { Button } from 'react-bootstrap';
 
 class App extends Component {
-  render() {
+  render () {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <div className='App'>
+        <div className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
           <h2>Welcome to foodbook</h2>
         </div>
+        <form>
+          <label style={{ borderRaduis: 3, color: 'purple'}}>
+            <input placeholder="enter username"  style={{borderWidth:1, borderColor: 'purple' }}type='text' name='name' />
+          </label>
+          <br/>
+          <label style={{color: 'purple'}}>
+            <input placeholder="enter username"  style={{borderWidth:1, borderColor: 'purple' }}type='text' name='name' />
+          </label>
+          <br/>
+          <input className= 'App-login' type='submit' value='Submit' />
+        </form>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
